5d43339c4796fa3c7e4fd371d4b63453db9612f8
root <2844204842@qq.com>
2023-02-17 13:06:10 +0800

    second commit

--- src/main/java/com/ljq/git/GitTest.java
+++ src/main/java/com/ljq/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ljq.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        System.out.println("hello git2 !");
     }
 }
